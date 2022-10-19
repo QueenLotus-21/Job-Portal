f22d9cdd094a32fdd47a94f0d1323549337efd43
QueenLotus156 <tigistamakelew156@gmail.com>
2022-10-19 04:44:19 -0700

    change user password

--- frontend/src/app/services/token-interceptor.service.ts
+++ frontend/src/app/services/token-interceptor.service.ts
@@ -12,7 +12,7 @@ export class TokenInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
    //let token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNjY1MjU3Mzk5LCJleHAiOjE2NjUyNjA5OTksIm5iZiI6MTY2NTI1NzM5OSwianRpIjoiSlFwSWZjWGh6RnlONXd0SCIsInN1YiI6IjEiLCJwcnYiOiIzNTcwMTczZDc0NDhjZGIwMjc5MGMxOTdlOTFkOTkyOTk5YWMxMGZiIn0.tfdhCx37_tMuVgGtHGRDOSuGY4xtmvDzySAaFwrK1tY'
   let Auth=this.injector.get(UserServiceService)
-
+// Authorization: `bearer ${Auth.getToken()}`,
    let jwtoken=req.clone({
       setHeaders:{
         Authorization: `bearer`+Auth.getToken(),
